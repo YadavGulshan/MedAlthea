1270461930bbe1463e1e0b7f8962735d13d3d0fc
rahul007-bit <yadavrahul8141@yahoo.com>
2022-01-22 16:11:09 +0530

    add model and serializers

--- services/pharmacy/models.py
+++ services/pharmacy/models.py
@@ -2,26 +2,24 @@ from django.db import models
 from djangotoolbox.fields import ListField
 # Create your models here.
 
-class medicineDetailsModel(models.Model):
+class medicineDetails(models.Model):
     medicine_name = models.CharField(max_length=100)
     medicine_price = models.FloatField(max_length=50)
     medicine_rating = models.FloatField(max_length=5)
     medicine_total_rating = models.IntegerField(max_length=100)
     medicine_noOfTablets = models.IntegerField(max_length=20)
+    class Meta:
+        abstract = True
 
 
-
-
-
-class shopAndOwnerDetails(models.Model):
+class shopAndOwnerDetails(medicineDetails):
     owner_first_name = models.CharField(max_length=30)
     owner_last_name = models.CharField(max_length=30)
     owner_email = models.EmailField(max_length=20)
-    owner_phone = models.IntegerField(max_length=10)
     shop_name = models.CharField(max_length=15)
     shop_address = models.CharField(max_length=50)
     shope_District = models.CharField(max_length=10)
     shop_state = models.CharField(max_length=10)
     shop_contact = models.IntegerField(max_length=10)
-    medicines = ListField()
+    medicines = ListField(medicineDetails)
 
