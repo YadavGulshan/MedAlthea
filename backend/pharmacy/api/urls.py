9105b3c25a7d705b3fdf8feec0b6b623909217c8
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-29 15:31:34 +0530

    added an override class so that a token can contain both username and email addr

--- backend/pharmacy/api/urls.py
+++ backend/pharmacy/api/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from .views import MyTokenObtainPairView
 
 urlpatterns = [
     path('', views.getData),
     path('post/', views.postData),
     path('update/', views.updateData),
     path('get/', views.getRoutes),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- backend/pharmacy/api/views.py
+++ backend/pharmacy/api/views.py
@@ -4,6 +4,24 @@ from rest_framework.decorators import api_view
 from pharmacy.models import Medical
 from .serializers import MedicalSerializer
 
+# For customizing user claims
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['username'] = user.username
+        token['email'] = user.email
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 @api_view(['GET'])
 def getData(request):
     medical = Medical.objects.all()
