df335126a0f2f25c38a588d824ea3193b10c6cd0
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-30 21:36:53 +0530

    registered the url patter for registering the user and also added some comments

--- backend/pharmacy/api/urls.py
+++ backend/pharmacy/api/urls.py
@@ -13,15 +13,22 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
+    # For getting the api view: i.e medical list and all
     path('', views.DataList.as_view(), name='get'),
-    path('<int:pk>/', views.Data.as_view()),
+    path('<int:pk>/', views.Data.as_view(), name='get'),
 
     # User
-    path('user/', views.User.as_view()),
+    path('user/', views.User.as_view(), name='user'),
 
+    ## Auth
+
+    # Auth: login
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    # Auth: register user
+    path('register/', views.Register.as_view(), name='register'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
