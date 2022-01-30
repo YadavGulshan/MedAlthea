c5279b31a05621e2b2369fa9640cbe873821c055
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-30 21:49:29 +0530

    Hmm, I missed that password validation

--- backend/pharmacy/api/serializers.py
+++ backend/pharmacy/api/serializers.py
@@ -29,17 +29,22 @@ class RegisterSerializer(serializers.ModelSerializer):
             'last_name': {'required': True}
         }
 
+    def validatePassword(self, attr):
+        if attr['password'] !=attr['password2']:
+            raise serializers.ValidationError('Passwords must match')
+        return attr
 
-        def validate(self, validated_data):
-            user = User.objects.create(
-                username=validated_data['username'],
-                email=validated_data['email'],
-                first_name=validated_data['first_name'],
-                last_name=validated_data['last_name']
-            )
 
-            # Set the user to staff
-            user.is_staff = True
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
+        
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        # Set the user to staff
+        user.is_staff = True
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
