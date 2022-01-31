6e414b7ff53ba3e1ccfa63db4352253d2a27f1c3
YadavGulshan <yadavgulshan542@gmail.com>
2022-01-31 15:36:49 +0530

    added licensing stuff

--- backend/pharmacy/admin.py
+++ backend/pharmacy/admin.py
@@ -1,3 +1,13 @@
+# pylint: disable=missing-module-docstring
+# 
+# Copyright (C) 2022 by YadavGulshan@Github, < https://github.com/YadavGulshan >.
+#
+# This file is part of < https://github.com/Yadavgulshan/pharmaService > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YadavGulshan/pharmaService/blob/master/LICENCE >
+#
+# All rights reserved.
+
 from django.contrib import admin
 
 # Register your models here.
