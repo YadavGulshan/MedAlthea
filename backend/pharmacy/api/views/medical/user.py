6126544d14b75809374a5ae5c4903d082f026a83
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-07 18:27:05 +0530

    Added a new route for getting the list of listed medical shops by user

---
+++ backend/pharmacy/api/views/medical/user.py
@@ -0,0 +1,28 @@
+# pylint: disable=missing-module-docstring
+# 
+# Copyright (C) 2022 by YadavGulshan@Github, < https://github.com/YadavGulshan >.
+#
+# This file is part of < https://github.com/Yadavgulshan/pharmaService > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/YadavGulshan/pharmaService/blob/master/LICENCE >
+#
+# All rights reserved.
+
+from rest_framework import generics
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from pharmacy.api.serializers import MedicalSerializer
+from pharmacy.models import Medical
+
+@permission_classes([IsAuthenticated])
+class MyMedical(generics.ListCreateAPIView):
+    serializer_class = MedicalSerializer
+
+    def get(self, request):
+        medical = Medical.objects.filter(user=request.user)
+        serializer = MedicalSerializer(medical, many=True)
+        return Response(serializer.data)
+
+    
