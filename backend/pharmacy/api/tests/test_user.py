6ecdbcf79dce50d6f0e98b3561ee3aba9e839538
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-07 21:52:28 +0530

    removed the useless for loop and also fixed the test comparing wrong status code

--- backend/pharmacy/api/tests/test_user.py
+++ backend/pharmacy/api/tests/test_user.py
@@ -21,31 +21,34 @@ class userAuthTest(APITestCase):
         self.factory = APIRequestFactory()
         self.client = APIClient()
 
-        for i in range(10):
-            User.objects.create_user(
-                username="testuser" + str(i),
-                password="top_secret" + str(i),
-                email="testuser" + str(i) + "@email.com",
-                first_name="test",
-                last_name="user",
-            )
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="top_secret",
+            email="testuser@email.com",
+            first_name="test",
+            last_name="user",
+        )
 
     def test_user_login(self):
         response = self.client.post(
-            "/api/token/", {"username": "testuser1", "password": "top_secret1"}
+            "/api/token/", {"username": "testuser", "password": "top_secret"}
         )
         self.assertEqual(response.status_code, 200)
 
     def test_unauthenticated_user(self):
         response = self.client.post(
-            "/api/token/", {"username": "testuser1", "password": "wrong_password"}
+            "/api/token/", {"username": "testuser1",
+                            "password": "wrong_password"}
         )
         self.assertEqual(response.status_code, 401)
 
     def test_username_exist(self):
-        response = self.client.get("/api/register/search/?username=testuser1")
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get("/api/register/search/?username=testuser")
+        self.assertEqual(response.status_code, 400)
 
     def test_username_not_exist(self):
-        response = self.client.get("/api/register/search/?username=testuser")
+        response = self.client.get("/api/register/search/?username=testuser1")
         self.assertEqual(response.status_code, 200)
+
+    def test_refresh_token(self):
+        pass
