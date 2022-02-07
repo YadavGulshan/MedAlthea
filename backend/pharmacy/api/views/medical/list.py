5352401da103c0cfed623c389686209c6b1cf7f2
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-07 17:39:56 +0530

    Fixed the api route with serializer class

--- backend/pharmacy/api/views/medical/list.py
+++ backend/pharmacy/api/views/medical/list.py
@@ -11,15 +11,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticated
+from pharmacy.api.views.userActions.token import serializer
 
 from pharmacy.models import Medical
 from ...serializers import MedicalSerializer
 
-
+from rest_framework import generics
 from rest_framework.views import APIView
 
 @permission_classes([IsAuthenticated])
-class MedicalViewList(APIView):
+class MedicalViewList(generics.CreateAPIView):
+    serializer_class = MedicalSerializer
     def get(self, request):
         medical = Medical.objects.all()
         serializer = MedicalSerializer(medical, many=True)
@@ -27,13 +29,6 @@ class MedicalViewList(APIView):
 
     
     def post(self, request):
-        """
-        This section almost ripped me apart. I don't know why this section 
-        is not working now without making serializer mutable by literraly copying 
-        the data.
-
-        Bruh... 
-        """
         serializer = MedicalSerializer(data=request.data)
         # Make serializer mutable
         serializer.initial_data = serializer.initial_data.copy()
