1c73de58e059e1e51375f138dfcb6e53268f8e47
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-12 17:31:59 +0530

    made the setup medical shop less cluttery

--- backend/pharmacy/api/tests/setup/setup_medical_shop.py
+++ backend/pharmacy/api/tests/setup/setup_medical_shop.py
@@ -20,29 +20,41 @@ class setupMedical:
         }
 
     def setupMedicalShop(client: APIClient, header: str, **kwargs):
-        payload = {
-            "name": kwargs.get("name") != None and kwargs.get("name") or "Test Medical",
-            "address": kwargs.get("address") != None
+        name = (kwargs.get("name") != None and kwargs.get("name") or "Test Medical",)
+        address = (
+            kwargs.get("address") != None
             and kwargs.get("address")
             or "TestUser Medical Shop Address",
-            "pincode": kwargs.get("pincode") != None
-            and kwargs.get("pincode")
-            or 400607,
-            "phone": kwargs.get("phone") != None
-            and kwargs.get("phone")
-            or "+911234567891",
-            "latitude": kwargs.get("latitude") != None
-            and kwargs.get("latitude")
-            or random(),
-            "longitude": kwargs.get("longitude") != None
-            and kwargs.get("longitude")
-            or random(),
-            "email": kwargs.get("email") != None
+        )
+        pincode = (kwargs.get("pincode") != None and kwargs.get("pincode") or 400607,)
+        phone = (
+            kwargs.get("phone") != None and kwargs.get("phone") or "+911234567891",
+        )
+        latitude = (
+            kwargs.get("latitude") != None and kwargs.get("latitude") or random(),
+        )
+        longitude = (
+            kwargs.get("longitude") != None and kwargs.get("longitude") or random(),
+        )
+        email = (
+            kwargs.get("email") != None
             and kwargs.get("email")
-            or "testuser@email.com",
-            "website": kwargs.get("website") != None
+            or "testuser" + "@email.com",
+        )
+        website = (
+            kwargs.get("website") != None
             and kwargs.get("website")
-            or "https://testuser.com",
+            or "https://testuser" + ".com",
+        )
+        payload = {
+            "name": name,
+            "address": address,
+            "pincode": pincode,
+            "phone": phone,
+            "latitude": latitude,
+            "longitude": longitude,
+            "email": email,
+            "website": website,
         }
         response = client.post("/api/", payload, HTTP_AUTHORIZATION=header)
         return response
