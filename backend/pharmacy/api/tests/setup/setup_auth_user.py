4b81be93a7aeefadc469c84e089b38029b722873
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-12 17:29:00 +0530

    Modified user setup with kwargs

--- backend/pharmacy/api/tests/setup/setup_auth_user.py
+++ backend/pharmacy/api/tests/setup/setup_auth_user.py
@@ -5,19 +5,38 @@ from rest_framework.test import APIRequestFactory, APITestCase, APIClient
 
 
 class setup:
-    def setup_auth_user():
+    def setup_auth_user(**kwargs):
         factory = APIRequestFactory()
         client = APIClient()
+
+        username = str(
+            kwargs.get("username") != None and kwargs.get("username") or "testuser",
+        )
+        password = str(
+            kwargs.get("password") != None and kwargs.get("password") or "top_secret",
+        )
+        email = str(
+            kwargs.get("email") != None
+            and kwargs.get("email")
+            or "testemail@email.com",
+        )
+        first_name = str(
+            kwargs.get("first_name") != None and kwargs.get("first_name") or "Test",
+        )
+        last_name = str(
+            kwargs.get("last_name") != None and kwargs.get("last_name") or "User",
+        )
+
         user = User.objects.create_user(
-            username="testuser",
-            password="top_secret",
-            email="testuser@email.com",
-            first_name="test",
-            last_name="user",
+            username=username,
+            password=password,
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
             is_staff=True,
         )
         response = client.post(
-            "/api/token/", {"username": "testuser", "password": "top_secret"}
+            "/api/token/", {"username": username, "password": password}
         )
         access_token = response.data["access"]
         header = "Bearer " + access_token
