107dc0912da73440a68823047a400a693896d184
YadavGulshan <yadavgulshan542@gmail.com>
2022-02-12 17:20:54 +0530

    Added a test for checking the real owner of a medical

--- backend/pharmacy/api/tests/setup/setup_medical_shop.py
+++ backend/pharmacy/api/tests/setup/setup_medical_shop.py
@@ -19,16 +19,30 @@ class setupMedical:
             "website": "https://testuser" + ".com",
         }
 
-    def setupMedicalShop(client: APIClient, header: str):
+    def setupMedicalShop(client: APIClient, header: str, **kwargs):
         payload = {
-            "name": "TestUser",
-            "address": "TestUser Medical Shop Address",
-            "pincode": 400607,
-            "phone": "+911234567891",
-            "latitude": random(),
-            "longitude": random(),
-            "email": "testuser" + "@email.com",
-            "website": "https://testuser" + ".com",
+            "name": kwargs.get("name") != None and kwargs.get("name") or "Test Medical",
+            "address": kwargs.get("address") != None
+            and kwargs.get("address")
+            or "TestUser Medical Shop Address",
+            "pincode": kwargs.get("pincode") != None
+            and kwargs.get("pincode")
+            or 400607,
+            "phone": kwargs.get("phone") != None
+            and kwargs.get("phone")
+            or "+911234567891",
+            "latitude": kwargs.get("latitude") != None
+            and kwargs.get("latitude")
+            or random(),
+            "longitude": kwargs.get("longitude") != None
+            and kwargs.get("longitude")
+            or random(),
+            "email": kwargs.get("email") != None
+            and kwargs.get("email")
+            or "testuser@email.com",
+            "website": kwargs.get("website") != None
+            and kwargs.get("website")
+            or "https://testuser.com",
         }
         response = client.post("/api/", payload, HTTP_AUTHORIZATION=header)
         return response
--- backend/pharmacy/api/tests/unit/medicial/test_medical_user.py
+++ backend/pharmacy/api/tests/unit/medicial/test_medical_user.py
@@ -13,7 +13,19 @@ from rest_framework.test import APITestCase
 from pharmacy.api.tests.setup import Service
 
 
-class MedicalOwner:
-    def setup(self):
+class MedicalOwner(APITestCase):
+    def setUp(self):
         self.factory, self.client, self.header = Service.setup_auth_user()
-        pass
+        Service.setupMedicalShop(self.client, self.header)
+        Service.setupMedicalShop(
+            self.client,
+            self.header,
+            name="Pawan Medical",
+            email="pawan@email.com",
+            phone="+912626333322",
+        )
+
+    def test_owner_owns(self):
+        response = self.client.get("/api/mymedical/", HTTP_AUTHORIZATION=self.header)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 2)
