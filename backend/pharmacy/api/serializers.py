42041635f5a03d35fc01b909b865fb4928a670fb
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-16 18:14:06 +0530

    wrote test for login user

--- backend/pharmacy/api/serializers.py
+++ backend/pharmacy/api/serializers.py
@@ -107,9 +107,9 @@ class RegisterSerializer(serializers.ModelSerializer):
             first_name=validated_data["first_name"],
             last_name=validated_data["last_name"],
         )
-        # Set the user to staff
-        user.is_staff = True
-        user.set_active = True
+        # # Set the user to staff
+        # user.is_staff = True
+        # user.set_active = True
         user.set_password(validated_data["password"])
         user.save()
 
