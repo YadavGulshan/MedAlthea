1113dd75ae8c573f0cc239d5a061fa6f1b0cf2dd
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-30 16:53:34 +0530

    added a package for handling auth

---
+++ medalthea/packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,7 @@
+/// {@template authentication_repository}
+/// Authentication repository
+/// {@endtemplate}
+class AuthenticationRepository {
+  /// {@macro authentication_repository}
+  const AuthenticationRepository();
+}
