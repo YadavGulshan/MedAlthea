e25e32945b99b0f00a52118ee4d9c9a9b1386d4c
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-30 17:09:00 +0530

    added cache client

--- medalthea/packages/cache/test/src/cache_test.dart
+++ medalthea/packages/cache/test/src/cache_test.dart
@@ -1,11 +1,18 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:cache/cache.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+// ignore: avoid_relative_lib_imports
+import '../../lib/src/cache.dart';
+
 void main() {
-  group('Cache', () {
-    test('can be instantiated', () {
-      expect(Cache(), isNotNull);
+  group('CacheClient', () {
+    test('can write and read a value for a given key', () {
+      final cache = CacheClient();
+      const key = '__key__';
+      const value = '__value__';
+      expect(cache.read(key: key), isNull);
+      cache.write(key: key, value: value);
+      expect(cache.read(key: key), equals(value));
     });
   });
 }
